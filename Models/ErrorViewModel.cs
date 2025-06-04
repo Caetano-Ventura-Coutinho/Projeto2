95fdf99f6661934e2882c20c5c0c1d57ec369af8
Caetano <currytano@gmail.com>
2025-06-04 08:16:18 -0300

    Criando o Projeto Projeto2

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROJETO2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
